33545296d3451ade8ca193d96942b8aab4033927
Clay Palumbo <claypalumbo@clays-mbp.home>
2018-12-29 11:15:55 -0500

    Grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Col, Row, Container } from "reactstrap";
+import { Col, Row, Container } from 'reactstrap';
 import ScoreHeader from './components/ScoreHeader';
 import Header from './components/Header';
 import BoxScoreLeft from './components/BoxScoreLeft';
@@ -9,51 +9,36 @@ import BoxScoreRight from './components/BoxScoreRight';
 import AdOne from './components/AdOne';
 import Footer from './components/Footer';
 
-
-
 class App extends Component {
   render() {
-    return (
-<Container fluid>
-  <Row>
-      <Header />
-  </Row>
-  <Row>
-    <Col lg="12" className="centerss">
-     <ScoreHeader />
-    </Col>
-  </Row>
-  <Row>
-    <Col md="6" className="centerss mt-5">
-     <AdOne />
-    </Col>
-  </Row>
+    return <Container fluid>
+        <Row>
+          <Header />
+        </Row>
+        <Row>
+          <Col lg='12' className='centerss'>
+            <ScoreHeader />
+          </Col>
+        </Row>
+        <Row>
+          <Col md='6' className='centerss mt-5'>
+            <AdOne />
+          </Col>
+        </Row>
 
-  <Row>
-    <Col sm="4" className="centerss mb-5 mt-5 border1">
-     <BoxScoreLeft />
-     </Col>
-  </Row>
-  <Row>
- <Col lg="12" className="centerss">
-   <Row>
-       <Col lg='6' >test</Col>
-       <Col lg='6' >test</Col>
-   </Row>
- </Col>
-</Row>
-  <Row>
-  <Col sm="4" className="centerss mb-5 mt-5 border2">
-     <BoxScoreRight />
-     </Col>
-     </Row>
-  <Row>
-      <Footer />
-  </Row>
-
-
-</Container>
-    );
+        <Row>
+          <Col sm='4' className='centerss pr-0 mr-0 mb-5 mt-5 border1'>
+            <BoxScoreLeft />
+          </Col>
+          <Col sm='4' className='centerss pl-0 ml-0 mb-5 mt-5 border2'>
+            <BoxScoreRight />
+          </Col>
+        </Row>
+        <Row />
+        <Row>
+          <Footer />
+        </Row>
+      </Container>;
   }
 }
 
